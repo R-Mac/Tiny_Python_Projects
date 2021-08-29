084aa38598377d4768fc8cfad16aaf5e37481f31
r macrobbie <ryanmacrobbie@hotmail.com>
2021-08-28 19:00:53 -0600

    added get_args definition

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -7,11 +7,22 @@ being thorough :)
 
 Version2: added the argument parsing module in order to handle -h
 
+Version3: added the def main and __name__ check as is standard with most 
+well written python programs. This is done in case 
+
+Version4: added seperate get_args() function to process arguments
 """
 import argparse
 
-parser = argparse.ArgumentParser(description="Say Hello")
-parser.add_argument('-n','--name',metavar='name',default='World',help='Name to greet',)
-args = parser.parse_args()
+def get_args():
+    parser = argparse.ArgumentParser(description="Say Hello")
+    parser.add_argument('-n','--name',metavar='name',default='World',help='Name to greet',)
+    args = parser.parse_args()
 
-print('Hello, ' + args.name + '!') # This also doubles as the commenting tutorial.
+def main():
+    args = get_args()
+
+    print('Hello, ' + args.name + '!') # This also doubles as the commenting tutorial.
+
+if __name__ == '__main__':
+    main()
